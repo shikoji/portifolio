d10b7c667f7ea224771981af814044206119d723
Shikoji <robsongomes@acad.ifma.edu.br>
2025-06-30 12:06:02 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -21,7 +21,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const hash = window.location.hash.replace("#", "") || "home";
   showPage(hash);
+
+  // NOVO: SPA funcionando com os links do hamburguer também
+  const hamburgerLinks = document.querySelectorAll(".navigation a");
+  const hamburgerCheckbox = document.getElementById("toggleCheck");
+
+  hamburgerLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").replace("#", "");
+      showPage(targetId);
+      history.pushState(null, "", `#${targetId}`);
+      hamburgerCheckbox.checked = false; // Fecha o menu
+    });
+  });
 });
 
+// animação de franks kafka
+const elementos = document.querySelectorAll('.um');
 
-
+elementos.forEach(el => {
+  const letras = el.textContent.split('');
+  el.innerHTML = letras.map((letra, i) =>
+    `<span style="display:inline-block; animation: balanco 1s ease-in-out infinite; animation-delay: ${i * 0.1}s">${letra}</span>`
+  ).join('');
+});
